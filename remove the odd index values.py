1a83654da3e6920c78f4c457e6bcdfb1371eaca2
PavankumarElango <elangopavankumar@gmail.com>
2022-05-30 08:15:47 +0530

    2

--- remove the odd index values.py
+++ remove the odd index values.py
@@ -6,8 +6,8 @@ Created on Sat May 21 21:43:23 2022
 """
 
 #remove the characters which have odd index values of a given string.
-
-word = input("enter word")
+on 2.0
+ssss = input("enter word")
 def odd_values_string(word):
     result =list()
     for index in range(len(word)):
